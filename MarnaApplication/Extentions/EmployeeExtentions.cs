ce082c8bc7723413cb4f63f34885b2f50a4e2a2a
ParsaValii <parsa.valizadee@gmail.com>
2024-02-24 00:05:54 +0330

    ExtentionMethodFixed

--- MarnaApplication/Extentions/EmployeeExtentions.cs
+++ MarnaApplication/Extentions/EmployeeExtentions.cs
@@ -8,13 +8,13 @@ namespace MarnaApplication.Extentions
 {
     public static class EmployeeExtentions
     {
-        public static void ChangeSalary(this Employee employee, double percentage)
+        public static void ChangeSalary(this Employee employee, decimal percentage)
         {
             employee.Salary += (employee.Salary * (decimal)percentage) / 100;
         }
         public static void ChangeEmployeeExperience(this Employee employee, int years)
         {
-            employee.Experience += years;
+            employee.Experience = years;
         }
     }
 }
